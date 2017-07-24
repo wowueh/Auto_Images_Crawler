d7a4b254fc2b6fde8cb91bdb5c99a5fb613b1048
Thanh Ho <wowueh@gmail.com>
2017-07-24 14:54:58 +0700

    Upgraded

--- AutoCrawlImageFromUrl/Program.cs
+++ AutoCrawlImageFromUrl/Program.cs
@@ -22,21 +22,25 @@ namespace AutoCrawlImageFromUrl
             imgCra.KichThuocFile = Convert.ToInt32(kichThuocFileInString);
             imgCra.AutoCrawlImageFromUrl();
             Console.ReadKey();
-        }        
+        }
     }
 
     class ImageCrawler
     {
         public string DuongLink { get; set; }
         public int KichThuocFile { get; set; }
+        public string FileType { get; set; }
 
         public void AutoCrawlImageFromUrl()
         {
             Task<string> htmlTask = RequestHtmlAsync(DuongLink);
             string html = htmlTask.Result;
-            List<string> imgUrls = FindImageUrl(html);
+            //List<string> imgUrls = FindImageUrl(html);
+            List<string> imgUrls = FindImageUrlBasedAhref(html);
             DownloadImageAsync(imgUrls);
+            Console.WriteLine("Chuong trinh dang chay ...");
         }
+
         // Request html document
         private async Task<string> RequestHtmlAsync(string diaChiFile)
         {
@@ -47,6 +51,7 @@ namespace AutoCrawlImageFromUrl
             var responseContent = await response.Content.ReadAsStringAsync();
             return responseContent;
         }
+
         // Find URLs of Images in html document which matching the minimum size of image
         private List<string> FindImageUrl(string target)
         {
@@ -54,55 +59,69 @@ namespace AutoCrawlImageFromUrl
             HtmlDocument doc = new HtmlDocument();
             doc.LoadHtml(target);
 
-
             // Now, using LINQ to get all Images
             List<HtmlNode> imageNodes = null;
             imageNodes = doc.DocumentNode.SelectNodes("//img").ToList();
             int nodeNum = imageNodes.Count;
-            Console.WriteLine(nodeNum);
+            Console.WriteLine("Trang web co {0} hinh anh.", nodeNum);
             List<string> imageUrls = new List<string>();
             foreach (HtmlNode node in imageNodes)
             {
                 string src = node.Attributes["src"].Value;
-                string httppattern = "^http.+jpg$";
+                string httppattern = "^http.+";
                 Regex httpregex = new Regex(httppattern);
                 if (httpregex.IsMatch(src))
                 {
                     imageUrls.Add(src);
+                    Console.WriteLine(src);
                 }
+                //else
+                //{
+                //    imageUrls.Add(DuongLink + src);
+                //    Console.WriteLine(DuongLink + src);
+                //}
             }
-            Console.WriteLine(imageUrls.Count());
-            //foreach (var item in imageUrls)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //Console.ReadKey();
-
-
-            //var imageNodes = doc.DocumentNode.Descendants("img")
-            //                   .Select(e => e.GetAttributeValue("src", null))
-            //                   .Where(s => !String.IsNullOrEmpty(s));
-            //Console.WriteLine(imageNodes);
-
-
-
-            //HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("//img");
-            //Console.WriteLine(linkNodes.Count);
-            //Console.ReadLine();
-            //foreach (HtmlNode linkNode in linkNodes)
-            //{
-            //    //HtmlAttribute link = linkNode.Attributes["href"];
-            //    HtmlNode imageNode = linkNode.SelectSingleNode("//img");
-            //    HtmlAttribute src = imageNode.Attributes["src"];
-
-            //   // string imageLink = link.Value;
-            //    string imageUrl = src.Value;
-            //    Console.WriteLine(imageUrl);
-            //}
-            //Console.ReadLine();
-
+            Console.WriteLine("Co {0} link anh http.", imageUrls.Count());
             return imageUrls;
         }
+
+        // Find URLs of Images base <a href>
+        private List<string> FindImageUrlBasedAhref(string target)
+        {
+            //Alert
+            Console.WriteLine("Su dung FindImageUrlBasedAhref!");
+            // Load the Html into the agility pack
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(target);
+
+            // Now, using LINQ to get all Images
+            List<HtmlNode> imageNodes = null;
+            imageNodes = doc.DocumentNode.SelectNodes("//a[@href]").ToList();
+            int nodeNum = imageNodes.Count;
+            Console.WriteLine("Trang web co {0} <a href>.", nodeNum);
+            List<string> imageUrls = new List<string>();
+            foreach (HtmlNode node in imageNodes)
+            {
+                string href = node.Attributes["href"].Value;
+                string jpgpattern = ".+jpg$";
+                string httpPattern = "^http.+jpg$";
+                Regex jpgregex = new Regex(jpgpattern);
+                Regex httpregex = new Regex(httpPattern);
+                if (httpregex.IsMatch(href))
+                {
+                    imageUrls.Add(href);
+                    Console.WriteLine(href);
+                }
+                else if (jpgregex.IsMatch(href))
+                {
+                    imageUrls.Add(DuongLink + href);
+                    Console.WriteLine(DuongLink + href);
+                }
+            }
+            Console.WriteLine("Tim thay {0} link anh.", imageUrls.Count());
+            return imageUrls;
+        }
+
         // Download all files whose link is contained in a String Array
         private async void DownloadImageAsync(List<string> target)
         {
@@ -113,20 +132,31 @@ namespace AutoCrawlImageFromUrl
             int i = 1;
             foreach (var item in target)
             {
+                //Tim extension cua file anh
+                string fileTypePattern = "...$";
+                Regex fileTypeRegex = new Regex(fileTypePattern);
+                Match fileTypeMatch = fileTypeRegex.Match(item);
+                FileType = fileTypeMatch.Value;
+                //Request image using it's URL
                 var client2 = new HttpClient();
                 var response = new HttpResponseMessage();
                 response = await client2.GetAsync(item);
                 var responseContent = await response.Content.ReadAsByteArrayAsync();
-
-                var file = new FileStream(rootFolder+saveLocation+"/"+i+".jpg", FileMode.OpenOrCreate);
-                var bw = new BinaryWriter(file);
-                bw.Write(responseContent);
-                bw.Flush();
-                bw.Close();
-                Console.WriteLine(i+" Completed...");
-                i++;
+                var fileSize = response.Content.Headers.ContentLength;
+                //Save to disk
+                long kichThuocFile = KichThuocFile * 1024;
+                if (fileSize >= kichThuocFile && FileType != "gif")
+                {
+                    var file = new FileStream(rootFolder + saveLocation + "/" + i + "." + FileType, FileMode.OpenOrCreate);
+                    var bw = new BinaryWriter(file);
+                    bw.Write(responseContent);
+                    bw.Flush();
+                    bw.Close();
+                    Console.WriteLine("{0}.{1} Download completed...{2} Bytes", i, FileType, fileSize);
+                    i++;
+                }
             }
-            Console.WriteLine("Da hoan tat download hinh anh. Vi tri luu: " + rootFolder + saveLocation);
+            Console.WriteLine("Da hoan tat download " + (i - 1) + " hinh anh co kich thuoc tren {0} kBytes. Vi tri luu: " + rootFolder + saveLocation, KichThuocFile);
         }
     }
 }
